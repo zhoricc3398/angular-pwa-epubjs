d5eb762feac8c767741b4bea77a16f2af36bc50d
zhoricc <giorgi.sharabidze@ugt.ge>
2021-03-17 21:20:57 +0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import * as epubjs from '../../node_modules/epubjs/dist/epub.min.js';
+import { DataService } from './core/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-pwa-epubjs';
+export class AppComponent implements OnInit {
+  //
+  book = null;
+
+  //
+  width = 600;
+  height = 600;
+
+  //
+  Rendition;
+
+  //
+  subscription: Subscription = new Subscription();
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  //
+  load(): void {
+    //
+    const url: string =
+      'https://zhoricc3398.github.io/ugt-csp/assets/test.epub';
+
+    //
+    this.subscription.add(
+      this.dataService.getData(url).subscribe(
+        (x) => {
+          console.log(x, '--------------------------');
+
+          let firstFile = x;
+
+          if (window.FileReader) {
+            let reader: FileReader = new FileReader();
+
+            reader.onload = (e) => {
+              // console.log(e);
+              this.book = epubjs(firstFile);
+              this.Rendition = this.book.renderTo('area');
+              //getting cover
+              // this.book.coverUrl().then((result) => {
+              // document.getElementById('cover').setAttribute('src', result);
+              // });
+              this.Rendition.display();
+              /* Replace area with the id for your div to put the book in */
+            };
+
+            reader.readAsArrayBuffer(firstFile);
+
+            reader.onloadend = (e) => {
+              // console.log(book);
+            };
+          } else {
+            alert(
+              'Your browser does not support the required features. Please use a modern browser such as Google Chrome, or Mozilla Firefox'
+            );
+          }
+          // });
+        },
+        (error) => {
+          console.log(error, '-------------------------*');
+
+          // var test = new Blob(error.error.text);
+          // console.log(test);
+        }
+      )
+    );
+  }
 }
